1e3cfe4bd331873076e077d9e26a28ccc352e313
Jonathan Miranda <jmiran15@jhu.edu>
2022-11-12 16:08:23 -0500

    attendee remove

--- server/routes/attendees.js
+++ server/routes/attendees.js
@@ -3,24 +3,6 @@ const { createIndexes } = require("../models/Attendee.js");
 const Attendee = require("../models/Attendee.js");
 const router = express.Router();
 
-
-// get all attendees 
-router.get("/attendee", async (req, res, next) => {
-    try {
-        const attendees = await Attendee.find()
-        res.send(events)
-    } catch (e) {
-        res.send(e.message)
-    }
-});
-
-// get attendee by id
-//router.get("/attendee/:id", async (req, res, next) => {
-
-//});
-
-router.get("/attendee/:")
-
 // add a new attendee
 router.post("/attendee", async (req, res) => {
     const attendee = new Attendee({
