eabe0fbf1c82d0789a4dca9328ceda6e1dd3a47c
Jeffery Zhou <jefferyzhouehs@gmail.com>
2022-11-12 14:46:08 -0500

    write attendee schema

--- server/models/Attendee.ts
+++ server/models/Attendee.ts
@@ -1,7 +1,19 @@
-const mongoose = require("mongoose");
+import {Schema, model, connect} from 'mongoose';
 
-const AttendeeSchema = mongoose.Schema({
-	stuff: String,
+interface IAttendee {
+	email: string;
+	interests: string[]
+	phone?: number;
+	major: string;
+	year: number;
+}
+
+const AttendeeSchema = new Schema<IAttendee>({
+	email: {type: String, required: true},
+	interests: {type: [String], required: true},
+	phone: Number,
+	major: {type: String, required: true},
+	year: {type: Number, required: true}
 });
 
-module.exports = mongoose.model("Attendee", AttendeeSchema);
+export const User = model<IAttendee>("Attendee", AttendeeSchema);
